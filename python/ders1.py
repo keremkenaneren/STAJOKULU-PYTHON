f817728a002370b76e40edd5b18e3955078377b6
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-07-18 16:21:04 +0300

    first commit

---
+++ python/ders1.py
@@ -0,0 +1,28 @@
+"""
+a = int(input("Sayi  1 : "))
+b = int(input("Sayi  2 : "))
+c = int(input("Sayi  3 : "))
+
+print("multiply : ", a*b*c)
+print("sum : ", a+b+c)
+print("avarage : ", (a+b+c)/3)
+
+"""
+
+
+"""
+kisaKenar = int(input("kısa kenar : "))
+uzunKenar = int(input("uzun kenatr : "))
+
+print("dikdörtgenin çevresi : ", (uzunKenar+kisaKenar)*2)
+print("Dikdörtgenin alanı = ", kisaKenar*uzunKenar)
+
+"""
+
+vize1 = int(input("vize 1 : "))
+vize2 = int(input("vize 2 : "))
+final = int(input("final : "))
+
+ortalama = (vize1*30/100) + (vize1*30/100) + (vize1*40/100)
+
+print(ortalama)
